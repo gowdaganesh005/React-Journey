728bf10fa6d0e16e62119075c322f806c2f5e7bc
Ganesh gowda <gowdaganesh0511@gmail.com>
2024-08-01 23:31:00 +0530

    env config file management

---
+++ 12BlogWeb/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  // console.log(import.meta.env.VITE_APPWRITE_ID)
+
+  return (
+    <>
+   <h1>Blog app using the appwrite backend</h1>
+    </>
+  )
+}
+
+export default App
